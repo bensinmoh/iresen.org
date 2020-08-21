a10fdebc378c4a04233b5cf5b43c2facb499c9c9
Mouhcine BENMEZIANE <benmezianemail@gmail.com>
2020-08-21 23:58:38 +0100

    projects

--- js/plugins.js
+++ js/plugins.js
@@ -21,8 +21,74 @@ if (localStorage.getItem('cookieConsentIRESEN') == "false") {
 
 // Slick Sliders
 
+$(document).ready(function(){ // Recent Projects
+  $('.slider-recent-projects').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [{
+      breakpoint: parseInt(computedStyle.getPropertyValue('--minSizeDesktop')),
+      settings: {
+        slidesToShow: 4,
+      }
+    }, {
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizeLarge')),
+      settings: {
+        slidesToShow: 3,
+      }
+    }, {
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizeTablet')),
+      settings: {
+        slidesToShow: 2,
+      }
+    }, {
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizePhone')),
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  })
+  .on('setPosition', function (event, slick) {
+    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+  });
+});
+
+$(document).ready(function(){ // Team
+  $('.slider-team').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [{
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizeLarge')),
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }, {
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizeTablet')),
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: parseInt(computedStyle.getPropertyValue('--maxSizePhone')),
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  })
+  .on('setPosition', function (event, slick) {
+    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+  });
+});
+
 $(document).ready(function(){ // Alliances
-  $('.alliances-logos').slick({
+  $('.slider-alliances-logos').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
@@ -49,7 +115,7 @@ $(document).ready(function(){ // Alliances
 });
 
 $(document).ready(function(){ // News
-  $('.recent-news').slick({
+  $('.slider-recent-news').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
